d6f823926a303603e279366c042948c3d6a43b35
tahajaffri123 <tahajaffri87@gmail.com>
2019-06-07 12:32:20 +0530

    loops

---
+++ ravi/php/foreach3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+
+$data1=array("name"=>"rohit", "age"=>25);
+
+
+
+foreach ($data1 as $a) {
+
+	echo $a;
+    
+	
+}
+
+
+
+	?>
+
+
+</body>
+</html>
---
+++ ravi/php/while.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+
+$i=1;
+
+while ($i<=10) {
+	echo"hello<br>";
+	$i++;
+}
+
+	?>
+
+</body>
+</html>
