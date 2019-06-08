9ff2a2c58e6fb1a650c20ae2ecbb07ac0ceed41e
bodana007 <meghasahu954@gmail.com>
2019-06-08 14:25:24 +0530

    today my proccess

---
+++ project/login.php
@@ -0,0 +1,46 @@
+<?php
+include("header.php");
+include("slider.php");
+?>
+
+<div class="conatiner-fluid">
+	<div class="container">
+		<ul class="nav justify-content-center center-nav">
+			<li class="nav-item">
+				<a href="#" class="nav-link">New Arrival</a>
+			</li>
+			<li class="nav-item slash">
+				/
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link">Recommended</a>
+			</li>
+			<li class="nav-item slash">
+				/
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link">Best Seller</a>
+			</li>
+			
+		</ul>
+	</div>
+</div>
+<div class="conatiner-fluid">
+	<div class="container">
+		<div class="row">
+			<?php include("left-menu.php"); ?>
+			<div class="col-md-9">
+				<div class="row">
+					<h3>User Login</h3>
+					<div class="card">
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+include("footer.php");
+?>
+
