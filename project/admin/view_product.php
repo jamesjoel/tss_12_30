9051ea3e194e2d9c04331572bfb90de2ce36a4b8
jamesjoel <james.joelj@gmail.com>
2019-06-17 14:23:31 +0530

    file upload in project

--- project/admin/view_product.php
+++ project/admin/view_product.php
@@ -13,13 +13,14 @@ $result = mysqli_query($con, $query);
 	<div class="container" style="min-height: 600px;">
 		<h2>View All Category</h2>
 		<div class="row">
-			<div class="col-md-10 offset-md-1">
+			<div class="col-md-12">
 				<table class="table table-bordered table-dark">
 					<tr>
 						<th>S.No.</th>
 						<th>Product Name</th>
 						<th>Product Price</th>
 						<th>Product Category</th>
+						<th>Product Image</th>
 						<td>View</td>
 						<td>Edit</td>
 						<td>Delete</td>
@@ -32,6 +33,7 @@ $result = mysqli_query($con, $query);
 							<td><?php echo $data['product_name'];?></td>
 							<td><?php echo $data['product_price'];?></td>
 							<td><?php echo $data['product_category'];?></td>
+							<td><img src="product_image/<?php echo $data['product_image']; ?>" height="100" width="100"/></td>
 							<td><a href="#" class="btn btn-info btn-sm">View</a></td>
 							<td><a href="#" class="btn btn-warning btn-sm">Edit</a></td>
 							<td><a href="#" class="btn btn-danger btn-sm">Delete</a></td>
