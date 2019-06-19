8fb89a2d5f9adf14efb62e234f04247a58cf6226
jamesjoel <james.joelj@gmail.com>
2019-06-19 14:11:05 +0530

    delete folders

--- brij/newupdate/project/view_product.php
+++
@@ -1,3 +0,0 @@
-<?php
-include("adminheader.php");
-?>
--- nilesh/photo_update.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<form action="save.php" method="post" ></form>
-</body>
-</html>
