fbf13079a16998829b9d3cb3e969c885467e2703
tahajaffri123 <tahajaffri87@gmail.com>
2019-07-11 13:12:27 +0530

    ajax validation

---
+++ ravi/ajax/state.php
@@ -0,0 +1,54 @@
+<?php
+$con = mysqli_connect("localhost", "admin", "admin", "tss_12_30");
+
+$query = "SELECT * FROM state";
+$res = mysqli_query($con, $query);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$("#state_dd").change(function(){
+				var a = $(this).val();
+				$.ajax({
+					url : "getcity.php",
+					type : "post",
+					data : { stateid : a},
+					success : function(res){
+						//alert(res);
+						$("#city_dd").html(res);
+					}
+				});
+			});
+		});
+	</script>
+</head>
+<body>
+<br />
+<br />
+Select State <select id="state_dd">
+				<option>Select</option>	
+				<?php
+				while($data=mysqli_fetch_assoc($res))
+				{ ?>
+					<option value="<?php echo $data['id']; ?>"><?php echo $data['state_name'];?></option>
+				<?php 
+				}
+				?>
+			</select>
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+Select City <select  id="city_dd">
+				<option>Select State First</option>
+			</select>
+</body>
+</html>
---
+++ ravi/project/login_old.php
@@ -0,0 +1,78 @@
+<?php
+include("db.php");
+include("header.php");
+include("slider.php");
+
+?>
+
+<div class="conatiner-fluid">
+	<div class="container">
+		<ul class="nav justify-content-center center-nav">
+			<li class="nav-item">
+				<a href="#" class="nav-link">New Arrival</a>
+			</li>
+			<li class="nav-item slash">
+				/
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link">Recommended</a>
+			</li>
+			<li class="nav-item slash">
+				/
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link">Best Seller</a>
+			</li>
+			
+		</ul>
+	</div>
+</div>
+<div class="conatiner-fluid">
+	<div class="container">
+		<div class="row">
+			<?php include("left-menu.php"); ?>
+			<div class="col-md-9">
+			<form action="auth.php" method="post">
+				
+			<h3>User Login</h3>
+				<div class="row">
+					<div class="col-md-8 offset-md-2">
+						<div class="card mb-4 shadow">
+							<div class="card-header">
+								<h2>Login</h2>
+							</div>
+							<div class="card-body">
+								<div class="form-group">
+									<label>Username</label>
+									<input type="text" class="form-control" name="username">
+								</div>
+								<div class="form-group">
+									<label>Password</label>
+									<input type="password" class="form-control" name="password">
+								</div>
+							</div>
+							<div class="card-footer">
+							<a href="my_account.php?id=<?php echo $data['id'];  ?>"><input type="submit" value="Login" class="btn btn-primary"><a>
+						<p class="text-danger text-center">
+							<?php
+							if(isset($_SESSION['msg']))
+							{
+								echo $_SESSION['msg'];
+								unset($_SESSION['msg']);
+								
+							}
+							?>
+						</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			</form>
+		</div>
+	</div>
+</div>
+<?php
+include("footer.php");
+?>
+
