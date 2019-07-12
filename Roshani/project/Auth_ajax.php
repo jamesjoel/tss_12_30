732de8280aa53883c1bca553f0deded22f39432f
patidaroshani <patidar.rashi@gmail.com>
2019-07-12 14:28:52 +0530

    login with ajax

---
+++ Roshani/project/Auth_ajax.php
@@ -0,0 +1,45 @@
+<?php
+// print_r($_POST);
+// $conn = mysqli_connect('localhost','root','','roshani');
+// session_start();
+include("dbcon.php");
+$u = $_POST['username'];
+$p  = $_POST['password'];
+$p = sha1($p);
+$query = "SELECT * from user WHERE username= '$u'";
+$result = mysqli_query($conn, $query);
+if(mysqli_num_rows($result)==1)
+{
+	$data = mysqli_fetch_assoc($result);
+       if($data['password']==$p)
+       {
+       	if ($data['status']==1) 
+       	{
+       	$_SESSION['name']= $data['full_name'];
+		$_SESSION['id']= $data['id'];
+		$_SESSION['is_user_logged_in']=true;
+	echo 1;
+       	
+       	}
+       	else
+       	{
+       		$_SESSION['msg']= "You Are Disabled now pls contact to admin !";
+			echo 2;
+
+       	}
+	
+		
+	}
+	    else
+	{
+		$_SESSION['msg']= "This Password is Incorrect !";
+	     echo 3;
+	}
+}
+		else
+{
+	
+	$_SESSION['msg'] = "This Username And Password is Incorrect !";
+		echo 4;
+}
+?>
